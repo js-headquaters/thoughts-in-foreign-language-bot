ee828c6d3138e51cf00854ab9645323d982132c3
Sharikov Vlad <sharikov.vladislav@gmail.com>
2025-05-22 17:36:42 -0300

    Updates

--- client/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { GameComponent } from "@components/game/game";
-import { render } from "preact";
-import "./style.css";
-
-render(<GameComponent />, document.getElementById("app"));
--- client/src/interfaces/context.ts
+++
@@ -1,8 +0,0 @@
-import { GameStateService } from "@services/game-state.service";
-import { StatisticService } from "@services/statistic.service";
-import { ThemeService } from "@services/theme.service";
-import { createContext } from "preact";
-
-export const GameStateContext = createContext<GameStateService>(null);
-export const StatisticContext = createContext<StatisticService>(null);
-export const ThemeContext = createContext<ThemeService>(null);
